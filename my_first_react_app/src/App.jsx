4806365ffbdc00ef3b6101878ca450179e3a59c1
haruna_hannah <harunahannah04@gmail.com>
2025-07-20 19:08:48 +0100

    my_first_react_app file created

--- my_first_react_app/src/App.jsx
+++ my_first_react_app/src/App.jsx
@@ -1,35 +1,134 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div>
+      <h2>My Learning Journey as a Web Developer</h2>
+
+      <p>I started learning web development in 2025 and it has been an amazing experience so far.
+        <br />
+        I began with HTML, then moved on to CSS and JavaScript. I'm currently learning React.
       </p>
-    </>
-  )
+      
+      
+        <h2>Technologies I have Learned</h2>
+        <ol>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+        </ol>
+
+        <h2>Topics I Enjoy Most</h2>
+        <ul>
+          <li>Designing Layouts</li>
+          <li>Responsive Web Design</li>
+          <li>Building Interactive Forms</li>
+        </ul>
+      
+    <table border="1">
+      <thead>
+        <tr>
+          <th>Technology</th>
+          <th>Learning Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>HTML</td>
+          <td>Completed</td>
+        </tr>
+        <tr>
+          <td>CSS</td>
+          <td>Completed</td>
+        </tr>
+        <tr>
+          <td>JavaScript</td>
+          <td>In Progress</td>
+        </tr>
+        <tr>
+          <td>React</td>
+          <td>In Progress</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <p>
+      Read more about React on <a href="https://react.dev" target="_blank" rel="noreferrer"> the official Reacr website</a>
+
+    </p>
+    <img 
+    scr="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" 
+              alt="Learning Journey" 
+              width="300"
+           /> 
+
+           <fieldset>
+              <legend>Contact Me</legend>
+
+              <form>
+                <label>
+                  Name:
+                  <input type="text" name="name" />
+                </label>
+                <br />
+
+                <label>
+                  Email:
+                  <input type="text" name="email" />
+                </label>
+                <br />
+
+                <label>
+                  Message:
+                  <br />
+                  <textarea name="message" rows="4" cols="30" />
+                </label>
+                <br />
+
+                <label>
+                  Gender:
+                  <input type="radio" name="gender" value="male" /> Male 
+                  <input type="radio" name="gender" value="female" /> Female
+                </label>
+                <br />
+
+                <label>
+                  Interest: 
+                  <input type="checkbox" value="coding" /> Coding
+                  <input type="checkbox" value="design" /> Design
+                </label>
+                <br />
+
+                <label>
+                  Age:
+                  <input type="number" name="age" />
+                </label>
+                <br />
+
+                <label>
+                  Favourite Language:
+                  <select name="language">
+                    <option value="html">HTML</option>
+                    <option value="css">CSS</option>
+                    <option value="javascript">JavaScript</option>
+                    <option value="react">React</option>
+                  </select>
+                </label>
+                <br />
+
+                <label>
+                  Start Date:
+                <input type="date" name="startdate"/>
+                </label>
+                <br />
+
+                <input type="submit" value="submit" />
+       </form>
+     </fieldset>            
+   </div>
+  );
 }
 
 export default App
