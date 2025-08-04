e9d60f61c1027934ccb3fbaff271e78429706421
haruna_hannah <harunahannah04@gmail.com>
2025-08-04 23:06:37 +0100

    react_with_tailwind file 2 created

--- react_with_tailwind/src/App.jsx
+++ react_with_tailwind/src/App.jsx
@@ -4,10 +4,21 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-        <p className='bg-blue-500'>Hello World</p>
+    <div className="min-h-screen flex items-center justify-center bg-gray-200 px-4">
+      <div className="bg-white p-6 sm:p-8 rounded-2xl shadow-xl w-full max-w-md">
+        <h1 className="text-blue-600 text-2xl sm:text-3xl font-bold mb-2">
+          HARUNA HANNAH ZENUM
+        </h1>
+      
+        <h2 className="text-green-900 text-lg sm:text-xl font-semibold mb-2">
+          FRONTEND DEVELOPER
+        </h2>
+        <p className="text-gray-500 text-sm sm:text-base font medium">
+          Passionate about building user-friendly interfaces and learning new technologies. I love creating clean and elegant designs with code. 
+          </p>
+        </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
