c2e313f18d83c5f3a6b6a9c162e5accc7e6c0264
haruna_hannah <harunahannah04@gmail.com>
2025-08-09 12:30:12 +0100

    react_with_tailwind file 4 created

--- react_with_tailwind/src/App.jsx
+++ react_with_tailwind/src/App.jsx
@@ -4,18 +4,22 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="p-10 space-y-6">
+    <div className="p-8 space-y-8">
         <h1 className="text-3xl font-bold mb-6 text-center">
           HARUNA HANNAH ZENUM
         </h1>
+        
+        <div className="bg-white p-4 m-4 border"></div>
         <p className="font-sans text-center underline capitalize"></p>
       
         <h2 className="text-xl font-semibold mb-2">
           FRONTEND DEVELOPER
         </h2>
-        <p className="text-sm font medium text-justify normal-case">
+        <div className="border border-red-500 p-4 rounded-lg">
+        <p className="text-sm font medium text-center normal-case">
           Passionate about building user-friendly interfaces and learning new technologies. I love creating clean and elegant designs with code. 
           </p>
+          </div>
     </div>
   );
 }
